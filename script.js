ab67321043a4ebbe2f7a5d89cd02035390a4bc09
brenoxdd4 <brenothegamer14@hotmail.com>
2022-09-15 22:49:28 -0300

    Fazendo funções

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var ul = document.querySelector('nav ul')
 var menuBtn = document.querySelector('.menu-btn i')
 var w = document.querySelector('body')
+var contact = document.querySelector('#acontact')
+var product = document.querySelector('#aproduct')
+var about = document.querySelector('#aabout')
+
+function onScroll() {
+  
+  console.log('scrollY')
+}
 
 function menuShow() {
   if (ul.classList.contains('open')) {
@@ -17,7 +25,17 @@ function hidden() {
   }
 }
 
+function menuClose() {
+  if (ul.classList.contains('open')) {
+    ul.classList.remove('open')
+  }
+}
+
 function menuShoww() {
   menuShow()
   hidden()
+}
+
+function all() {
+  onScroll()
 }
