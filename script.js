004a657059ff2839b583b336013dc5e3311b52a0
brenoxdd4 <brenothegamer14@hotmail.com>
2022-09-18 23:37:34 -0300

    arrumando navbar

--- script.js
+++ script.js
@@ -4,11 +4,50 @@ var w = document.querySelector('body')
 var contact = document.querySelector('#acontact')
 var product = document.querySelector('#aproduct')
 var about = document.querySelector('#aabout')
+var menu = document.querySelector('nav ul li a')
 
 function onScroll() {
-  
-  console.log('scrollY')
+  console.log(scrollY)
 }
+
+function HoverMenu() {
+  if (scrollY > 0) {
+    menu.classList.add('active')
+  }
+  if (scrollY > 1794) {
+    menu.classList.remove('active')
+  }
+}
+
+function HoverProduct() {
+  if (scrollY > 1795) {
+    product.classList.add('active')
+  }
+  if (scrollY > 3784) {
+    product.classList.remove('active')
+  }
+  if (scrollY > 1794) {
+    product.classList.remove('#active')
+  }
+}
+
+// function hovermenu() {
+//   if (scrollY > 0) {
+//     menu.classList.add('active')
+//   }
+//   if (scrollY > 1795) {
+//     menu.classList.remove('active')
+//   }
+// }
+
+// function hovermenu() {
+//   if (scrollY > 0) {
+//     menu.classList.add('active')
+//   }
+//   if (scrollY > 1795) {
+//     menu.classList.remove('active')
+//   }
+// }
 
 function menuShow() {
   if (ul.classList.contains('open')) {
@@ -38,4 +77,6 @@ function menuShoww() {
 
 function all() {
   onScroll()
+  HoverMenu()
+  HoverProduct()
 }
